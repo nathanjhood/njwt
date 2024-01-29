fd435de8a6eae0d1f6c0da1f7f121694706cd56f
StoneyDSP <nathanjhood@googlemail.com>
2024-01-29 04:14:53 +0100

    Added 'njwt' namespace and license info

---
+++ include/njwt/njwt.hpp
@@ -0,0 +1,44 @@
+/**
+ * @file njwt.hpp
+ * @author Nathan J. Hood   (nathanjhood@googlemail.com)
+ * @version 0.0.0
+ * @date 2023-12-29
+ *
+ * @copyright Copyright (C) 2024 Nathan J. Hood)
+ *
+ * This source code is provided 'as-is', without any express or implied
+ * warranty. In no event will the author be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this source code must not be misrepresented; you must not
+ *    claim that you wrote the original source code. If you use this source code
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original source code.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ */
+
+#ifndef NJWT_HPP_
+#define NJWT_HPP_
+
+// #include "cmrc.hpp"
+// #include "base64.hpp"
+
+#include "version.hpp"
+
+/**
+ * @brief the 'njwt' namespace.
+*/
+namespace njwt {
+
+} // namespace njwt
+
+#endif // NJWT_HPP_
